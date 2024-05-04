a1a35474dd3a13c194088c449ad1a3ae73d44224
Дмитрий Куликов <dkulikov@studio-tg.ru>
2024-05-04 10:59:36 +0300

    end lesson 1

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,16 @@
+import MyButton from "./components/MyButton/MyButton";
+
 function App() {
+    const user = {
+        name: "Дмитрий",
+        imageUrl: "/vite.svg",
+    }
   return (
     <>
-      Hello world
+      Hello world:
+        { user.name }
+        <img src={user.imageUrl} alt=""/>
+      <MyButton />
     </>
   )
 }
---
+++ src/components/MyButton/MyButton.tsx
@@ -0,0 +1,11 @@
+const MyButton = () => {
+    return (
+        <>
+            <button>Кнопка</button>
+            <button>Кнопка</button>
+            <h1>Привет мир</h1>
+        </>
+    );
+};
+
+export default MyButton;
