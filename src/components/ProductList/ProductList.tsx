244217c43a293d6f2df8ca37be390ce38ff25f6f
Дмитрий Куликов <dkulikov@studio-tg.ru>
2024-06-15 08:49:18 +0300

    после подключения react-query

--- src/components/ProductList/ProductList.tsx
+++ src/components/ProductList/ProductList.tsx
@@ -1,43 +1,26 @@
-import { FC, useEffect, useState } from "react";
-import { ProductItem } from "@/types";
+import { FC } from "react";
 import { Product, Row } from "@/components";
-import { api } from "@/app/api";
+import { useQuery } from "@tanstack/react-query";
+import { getProducts } from "@/services/products";
 
 const ProductList: FC = () => {
-  const [products, setProduct] = useState<ProductItem[]>([]);
-  const [isLoading, setIsLoading] = useState(false);
-  const [isError, setIsError] = useState(false);
-  const [error, setError] = useState("");
-
-  const getProduct = async () => {
-    setIsLoading(true);
-    try {
-      const { data } = await api.get("/products");
-      setProduct(data);
-    } catch (e) {
-      console.error(e);
-      setError(e as string);
-      setIsError(true);
-    }
-    setIsLoading(false);
-  };
-
-  useEffect(() => {
-    getProduct();
-  }, []);
+  const { data, isLoading, isError } = useQuery({
+    queryKey: ["products"],
+    queryFn: getProducts,
+  });
 
   if (isLoading) {
     return <h1>Загрузка</h1>;
   }
 
-  if (isError) {
-    return <h1>Ошибка.... {error}</h1>;
+  if (isError || !data) {
+    return <h1>Ошибка....</h1>;
   }
 
   return (
     <div>
       <Row direction="row">
-        {products.map((product) => (
+        {data.map((product) => (
           <Product key={product.id} product={product} />
         ))}
       </Row>
